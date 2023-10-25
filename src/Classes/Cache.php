b248bd811670195cf3867b0c8810544d3e37e8f8
Adaibekov Darkhan <78083548+dak0k@users.noreply.github.com>
2023-10-25 11:44:57 +0500

    More new classes
    Cache
    HttpClietn
    SessionManager

---
+++ src/Classes/Cache.php
@@ -0,0 +1,41 @@
+<?php
+/*
+--------------------------------------------------------------------------------
+---             Class created by dak0k "Adaibekov Darkhan"                   ---
+--------------------------------------------------------------------------------
+*/
+namespace src\Classes;
+
+class Cache {
+    private $cacheDir;
+
+    /**
+     * @param  mixed  $cacheDir
+     * @return void
+     */
+    public function __construct($cacheDir) {
+        $this->cacheDir = $cacheDir;
+    }
+
+    /**
+     * @param mixed $key
+     * @return mixed
+     */
+    public function get($key) {
+        $cacheFile = $this->cacheDir . '/' . $key;
+        if (file_exists($cacheFile) && time() - filemtime($cacheFile) < 3600) {
+            return unserialize(file_get_contents($cacheFile));
+        }
+        return false;
+    }
+
+    /**
+     * @param mixed $value
+     * @param mixed $value
+     * @return void
+     */
+    public function set($key, $value) {
+        $cacheFile = $this->cacheDir . '/' . $key;
+        file_put_contents($cacheFile, serialize($value));
+    }
+}
